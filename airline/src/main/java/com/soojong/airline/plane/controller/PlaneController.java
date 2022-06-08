74bf2247356caae4ee0ce3476d4b91f3a180dfe4
92SooJong <hsz3855@gmail.com>
2022-06-08 23:21:02 +0900

    [방법1]을 사용한 메소드 수행시간 측정. ( 메소드에 직접 측정 코드 작성 )

--- airline/src/main/java/com/soojong/airline/plane/controller/PlaneController.java
+++ airline/src/main/java/com/soojong/airline/plane/controller/PlaneController.java
@@ -3,14 +3,16 @@ package com.soojong.airline.plane.controller;
 import com.soojong.airline.plane.entity.Plane;
 import com.soojong.airline.plane.service.PlaneService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class PlaneController {
@@ -20,7 +22,13 @@ public class PlaneController {
     @GetMapping("/api/v1/planes")
     public ResponseEntity<List<Plane>> findAllPlanes(){
 
-        List<Plane> allPlanes = planeService.findAllPlanes();
+        log.info("{} 메소드 시작 " , "PlaneController.findAllPlanes");
+
+        long startTime = System.currentTimeMillis();
+        List<Plane> allPlanes = planeService.findAllPlanes(); // 핵심 로직
+        long endTime = System.currentTimeMillis();
+
+        log.info("{} 메소드 수행 소요시간 : {} 초" , "PlaneController.findAllPlanes", (float)(endTime-startTime)/1000 );
 
         return new ResponseEntity<>(allPlanes,HttpStatus.OK);
 
--- airline/src/main/java/com/soojong/airline/plane/service/PlaneService.java
+++ airline/src/main/java/com/soojong/airline/plane/service/PlaneService.java
@@ -3,10 +3,12 @@ package com.soojong.airline.plane.service;
 import com.soojong.airline.plane.entity.Plane;
 import com.soojong.airline.plane.repository.PlaneRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PlaneService {
@@ -15,7 +17,14 @@ public class PlaneService {
 
     public List<Plane> findAllPlanes() {
 
+        log.info("{} 메소드 시작 " , "PlaneService.findAllPlanes");
+
+        long startTime = System.currentTimeMillis();
         List<Plane> allPlanes = planeRepository.findAll();
+        long endTime = System.currentTimeMillis();
+
+        log.info("{} 메소드 수행 소요시간 : {} 초" , "PlaneService.findAllPlanes", (float)(endTime-startTime)/1000 );
+
 
         return allPlanes;
     }
