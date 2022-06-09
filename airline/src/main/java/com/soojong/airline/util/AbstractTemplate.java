2ae57b99e2c56215c4f4e2d080fff04feb1f3d4c
92SooJong <hsz3855@gmail.com>
2022-06-09 22:14:54 +0900

    [방법4] 진행중 - AbstractTemplate 생성

---
+++ airline/src/main/java/com/soojong/airline/util/AbstractTemplate.java
@@ -0,0 +1,22 @@
+package com.soojong.airline.util;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public abstract class AbstractTemplate<T> {
+
+    private final Watch watch;
+
+    public T execute(String methodName){
+
+        MethodTime methodTime = watch.startWatch(methodName);
+        T result = call();
+        watch.endWatch(methodTime);
+        return result;
+
+    }
+
+    protected abstract T call();
+
+
+}
