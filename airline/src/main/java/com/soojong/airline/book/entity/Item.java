b71b949ffd51c91bc17da6c7d9df163de1194ff8
92SooJong <hsz3855@gmail.com>
2022-07-19 00:27:54 +0900

    [245p] 조인 전략 예시

---
+++ airline/src/main/java/com/soojong/airline/book/entity/Item.java
@@ -0,0 +1,19 @@
+package com.soojong.airline.book.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "DTYPE")
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name; //이름
+    private int price; //가격
+
+}
+
+
