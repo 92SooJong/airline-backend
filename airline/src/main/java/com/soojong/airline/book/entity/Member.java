4d969474be50e5e58abcc41c91edeeeeed4e90dd
92SooJong <hsz3855@gmail.com>
2022-07-21 00:14:39 +0900

    [251p] MappedSuperclass 어노테이션 적용

---
+++ airline/src/main/java/com/soojong/airline/book/entity/Member.java
@@ -0,0 +1,12 @@
+package com.soojong.airline.book.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Member extends BaseEntity{
+
+    //ID 상속
+    //NAME 상속
+    private String email;
+
+}
